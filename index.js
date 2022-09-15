06f18e5ccc6bb79c82c33851265700be3e62ebdb
Alan Shen <alanshen111@gmail.com>
2022-09-15 12:49:04 -0700

    insertion

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
+// remember to inspect element to check console
+
 let button_sort = document.getElementById("button_sort");
+let select_algo = document.getElementById("select_algo");
 let bar_container = document.getElementById("bar_container");
 
 let num_bars = 20;
 let min_height = 2;
 let max_height = 20;
 let height_multiplier = 20; // allows more visually noticable difference between bar heights
-let delay_ms = 20;
+let delay_ms = 50;
 
-let unsorted_arr = new Array(num_bars);
+let my_array = new Array(num_bars);
+let current_algo = select_algo.value;
 let sorted = false;
 
 // calls function on load
 document.addEventListener("DOMContentLoaded", function() {
     randomizeArray();
-    renderBars(unsorted_arr);
+    renderBars(my_array);
+});
+
+select_algo.addEventListener("change", function () {
+    console.log(select_algo.value + " selected");
+    current_algo = select_algo.value;
 });
 
 button_sort.addEventListener("click", function () {
     if (sorted) {
         location.reload();
     } else {
-        let sorted_arr = bubbleSort(unsorted_arr);
-        console.log(sorted_arr);
+        console.log(current_algo + " running");
+        switch (current_algo) {
+            case "bubble": 
+                sortBubble(my_array); 
+                break;
+            case "insertion": 
+                sortInsertion(my_array);
+                break;
+            default: 
+                break;
+        }
         let button_sort_icon = document.getElementById("button_sort_icon");
         button_sort_icon.classList.remove("fa-play");
         button_sort_icon.classList.add("fa-repeat");
@@ -35,7 +53,7 @@ function getRandomNum(min, max) {
 
 function randomizeArray() {
     for (let i = 0; i < num_bars; i++) {
-        unsorted_arr[i] = getRandomNum(min_height, max_height);
+        my_array[i] = getRandomNum(min_height, max_height);
     }
 }
 
@@ -52,7 +70,7 @@ function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function bubbleSort(arr) {
+async function sortBubble(arr) {
     let bars = document.getElementsByClassName("bar");
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
@@ -83,4 +101,28 @@ async function bubbleSort(arr) {
     }
     return arr;
 }
+
+async function sortInsertion(array) {
+
+        let bars = document.getElementsByClassName("bar");
+        for (let i = 1; i < array.length; i++) {
+          let key = array[i];
+          let j = i - 1;
+          while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            bars[j + 1].style.height = array[j + 1] * height_multiplier + "px";
+            bars[j + 1].style.backgroundColor = "green";
+            await sleep(delay_ms);
+            bars[j + 1].style.backgroundColor = "black";
+            j = j - 1;
+          }
+          array[j + 1] = key;
+          bars[j + 1].style.height = array[j + 1] * height_multiplier + "px";
+          bars[j + 1].style.backgroundColor = "green";
+          await sleep(delay_ms);
+          bars[j + 1].style.backgroundColor = "black";
+        }
+      
+
+  }
 
